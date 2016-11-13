70f9372d5ef1aeb7cdf6a5afb914fd7e65bbb536
Patrick <patrick.lai+gitHub@lifeletters.com.au>
2016-11-14 02:26:33 +1100

    Fixed JS error
    Fixed JS error

--- app/js/creative.js
+++ app/js/creative.js
@@ -17,10 +17,13 @@
     });
 
     // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 51
-    })
+    if(typeof $('body').scrollspy){
+      console.log("scroll spy loaded");
+      $('body').scrollspy({
+          target: '.navbar-fixed-top',
+          offset: 51
+      })
+    }
 
     // Closes the Responsive Menu on Menu Item Click
     $('.navbar-collapse ul li a').click(function() {
